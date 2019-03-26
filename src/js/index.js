49ae2abb3cde1509f2ebdff8103d6c2190cbd9d3
Lundii <mondayemmanuel67@gmail.com>
2019-03-27 00:55:54 +0100

    Add SignUp, LogIn and Create Account form

---
+++ src/js/index.js
@@ -0,0 +1,28 @@
+function loginSignup(login){
+    document.querySelector("#registerCont").style.display = "none";
+    document.querySelector("#register").style.display = "none";
+    document.querySelector("#loginCont").style.display = "block";
+    document.querySelector("#signupCont").style.display = "block";
+
+    if (login){
+        document.querySelector("#login").style.display = "block";
+        document.querySelector("#loginCont").style.backgroundColor = "white";
+        document.querySelector("#signup").style.display = "none";
+        document.querySelector("#signupCont").style.backgroundColor = "rgb(240, 240, 240)";
+    }
+    else {
+        document.querySelector("#login").style.display = "none";
+        document.querySelector("#loginCont").style.backgroundColor = "rgb(240, 240, 240)";
+        document.querySelector("#signup").style.display = "block";
+        document.querySelector("#signupCont").style.backgroundColor = "white";
+    }
+}
+
+function registerUi(){
+    document.querySelector("#login").style.display = "none";
+    document.querySelector("#loginCont").style.display = "none";
+    document.querySelector("#signup").style.display = "none";
+    document.querySelector("#signupCont").style.display = "none";
+    document.querySelector("#register").style.display = "block";
+    document.querySelector("#registerCont").style.display = "block";
+}
