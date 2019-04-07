df252939cd94bc47a268a3fe0d0d4bb5b88ca84d
Lundii <mondayemmanuel67@gmail.com>
2019-04-07 22:50:41 +0100

    chore(testSetup): test for createBankAccount [Finishes #165155593]

--- src/routes/homeRoute.js
+++ src/routes/homeRoute.js
@@ -9,17 +9,6 @@ export default class HomeRouter {
     this.router.get('/', (req, res) => {
       res.sendFile(path.join(__dirname, '../../', 'index.html'));
     });
-
-    this.router.post('/create', (req, res) => {
-      const response = {
-        status: 200,
-        data: {
-          firstName: req.body.firstName,
-          lastName: req.body.lastName,
-        },
-      };
-      res.send(response);
-    });
     return this.router;
   }
 }
--- test/homeAPI_test/createAccount.js
+++ test/homeAPI_test/createAccount.js
@@ -1,36 +1,62 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable no-undef */
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import server from '../../server';
 
-const should = chai.should();
-const expert = chai.expect;
+const { expect } = chai;
 
 chai.use(chaiHttp);
 
-describe('/create API', () => {
-  it('for testing create account API', (done) => {
-    const body = {
-      firstName: 'Onu',
-      lastName: 'Monday',
-      email: 'mondayemmauel67@gmail.com',
-      sex: 'male',
-      phoneNumber: 90890987909,
-      dateOfBirth: new Date().getDate,
-    };
-    chai.request(server)
-      .post('/create')
-      .send(body)
-      .end((err, res) => {
-        res.should.have.status(200);
-        // res.should.be.a('object');
-        // res.body.should.have.property('status');
-        // res.body.should.have.property('data');
-        // res.body.data.should.have.property('id');
-        // res.body.data.should.have.property('token');
-        // res.body.data.should.have.property('firstName');
-        // res.body.data.should.have.property('lastName');
-        // res.body.data.should.have.property('email');
-        done();
-      });
+describe('Create Bank Account', () => {
+  describe('/POST create account number', () => {
+    it('should return a status 200 if all important fields are entered', (done) => {
+      const body = {
+        firstName: 'Onu',
+        lastName: 'Monday',
+        email: 'mondayemmauel67@gmail.com',
+        sex: 'male',
+        phoneNumber: '090890987909',
+        dateOfBirth: new Date().getDate,
+        type: 'saving',
+      };
+      chai.request(server)
+        .post('/create')
+        .send(body)
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          expect(res).to.be.a('object');
+          expect(res.body).to.have.all.keys('status', 'data');
+          expect(res.body.data).to.have.all.keys('accountNumber', 'firstName', 'lastName', 'email', 'type');
+          expect(res.body.data.accountNumber).to.be.a('Number');
+          expect(res.body.data.firstName).to.be.a('String');
+          expect(res.body.data.lastName).to.be.a('String');
+          expect(res.body.data.email).to.be.a('String');
+          expect(res.body.data.type).to.be.a('String');
+          done();
+        });
+    });
+  });
+  describe('/POST create account number', () => {
+    it('should return a status 400 if an important field is omited', (done) => {
+      const body = {
+        firstName: '',
+        lastName: 'Monday',
+        email: 'mondayemmauel67@gmail.com',
+        sex: 'male',
+        phoneNumber: '090890987909',
+        dateOfBirth: new Date().getDate,
+      };
+      chai.request(server)
+        .post('/create')
+        .send(body)
+        .end((err, res) => {
+          expect(res).to.have.status(400);
+          expect(res).to.be.a('object');
+          expect(res.body).to.have.all.keys('status', 'error');
+          expect(res.body.error).to.be.a('String');
+          done();
+        });
+    });
   });
 });
