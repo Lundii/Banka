3c33066c4695e388adfc977512c5feaa7b25184e
Lundii <mondayemmanuel67@gmail.com>
2019-04-07 23:16:53 +0100

    feature(register):create new account API [Start #165123700]

--- src/routes/homeRoute.js
+++ src/routes/homeRoute.js
@@ -9,6 +9,32 @@ export default class HomeRouter {
     this.router.get('/', (req, res) => {
       res.sendFile(path.join(__dirname, '../../', 'index.html'));
     });
+    this.router.post('/create', (req, res) => {
+      let result;
+      const {
+        firstName, lastName, email, type,
+      } = req.body;
+      if (!firstName.length || !lastName.length || !email.length || !type.length) {
+        res.status(400);
+        result = {
+          status: 400,
+          error: 'Please fill all required fields',
+        };
+      } else {
+        res.status(200);
+        result = {
+          status: 200,
+          data: {
+            accountNumber: 1235432678,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: 'somethihg@gmail.com',
+            type: 'current',
+          },
+        };
+      }
+      res.send(result);
+    });
     return this.router;
   }
 }
