b657b7048ef3c0f154aafeb42bb64ea3c722d4cd
Lundii <mondayemmanuel67@gmail.com>
2019-04-10 03:55:27 +0100

    feature(API):create user signup API [Finishes #165123164]

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
 import { debug } from 'debug';
 import config from './config';
 import routes from './src/routes';
@@ -7,12 +8,20 @@ import DB from './src/database/database';
 import storeLib from './src/models';
 
 const app = express();
+dotenv.config();
 const homeRouter = express.Router();
 
 const database = new DB('bankaApp');
 const store = {
   bankAcctStore: new storeLib.BankAcctStore('Bank Accounts', database),
+  userStore: new storeLib.UserStore('Users', database),
 };
+
+const StorageInfrastructure = new storeLib.StorageInfrastructure(store);
+
+function Init() {
+  StorageInfrastructure.init();
+}
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -27,6 +36,7 @@ const { port } = config.development;
 app.listen(port, (er) => {
   if (er) throw new Error('something is preventing the app from starting');
   serverlog(`app started on port ${port}`);
+  Init();
 });
 
 export default app;
--- src/routes/homeRoute.js
+++ src/routes/homeRoute.js
@@ -1,5 +1,7 @@
 /* eslint-disable no-trailing-spaces */
 import path from 'path';
+import { check } from 'express-validator/check';
+import Controllers from '../controllers';
 
 /**
  * Creates a router class for handling landing page APIs
@@ -15,6 +17,7 @@ export default class HomeRouter {
   constructor(router, store) {
     this.router = router;
     this.bankAcctStore = store.bankAcctStore;
+    this.homeController = new Controllers.HomeController(store);
   }
   
   /**
@@ -26,38 +29,26 @@ export default class HomeRouter {
         res.sendFile(path.join(__dirname, '../../', 'index.html'));
       });
     
-    this.router.get('/signup', (req, res) => {
-      res.send('my name is monday');
-    });
+    this.router.route('/signup')
+      .post([check('email', 'Please enter a valid email').isEmail()], this.homeController.signup);
 
     this.router.post('/create', (req, res) => {
       let result;
-      // const {
-      //   firstName, lastName, email, type,
-      // } = req.body;
-      // if (!firstName.length || !lastName.length || !email.length || !type.length) {
-      //   res.status(400);
-      //   result = {
-      //     status: 400,
-      //     error: 'Please fill all required fields',
-      //   };
-      // } else {
-        this.bankAcctStore.create(req.body, (err, data) => {
-          console.log('i sha got to this place');
-          console.log(data);
-        });
-        res.status(200);
-        result = {
-          status: 200,
-          data: {
-            accountNumber: 1235432678,
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            email: 'somethihg@gmail.com',
-            type: 'current',
-          },
-        };
-      // }
+      this.bankAcctStore.create(req.body, (err, data) => {
+        console.log('i sha got to this place');
+        console.log(data);
+      });
+      res.status(200);
+      result = {
+        status: 200,
+        data: {
+          accountNumber: 1235432678,
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          email: 'somethihg@gmail.com',
+          type: 'current',
+        },
+      };
       res.send(result);
     });
 
